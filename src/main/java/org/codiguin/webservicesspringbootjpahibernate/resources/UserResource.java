16da8dbbaee56351c654a9bc6ec71984bf0bbaa6
c0diguin <codiguin@git.com>
2025-08-05 18:19:44 -0300

    User entity and resource

---
+++ src/main/java/org/codiguin/webservicesspringbootjpahibernate/resources/UserResource.java
@@ -0,0 +1,19 @@
+package org.codiguin.webservicesspringbootjpahibernate.resources;
+
+import org.codiguin.webservicesspringbootjpahibernate.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Raskolnikov", "rodia@gmail.com", "99999-9999", "123456");
+
+        return ResponseEntity.ok().body(u);
+    }
+}
