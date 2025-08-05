d15c46f61230b0bc587824552bad6a6bee62840b
c0diguin <codiguin@git.com>
2025-08-05 17:51:30 -0300

    Project created

---
+++ src/main/java/org/codiguin/webservicesspringbootjpahibernate/WebservicesSpringbootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package org.codiguin.webservicesspringbootjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesSpringbootJpaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebservicesSpringbootJpaHibernateApplication.class, args);
+    }
+
+}
