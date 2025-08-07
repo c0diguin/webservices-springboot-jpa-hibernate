72669670f8c59575a29d6d3fcc01e4681decf0c7
c0diguin <codiguin@git.com>
2025-08-07 17:42:07 -0300

    Exception handling - findById

--- src/main/java/org/codiguin/webservicesspringbootjpahibernate/services/UserService.java
+++ src/main/java/org/codiguin/webservicesspringbootjpahibernate/services/UserService.java
@@ -2,6 +2,7 @@ package org.codiguin.webservicesspringbootjpahibernate.services;
 
 import org.codiguin.webservicesspringbootjpahibernate.entities.User;
 import org.codiguin.webservicesspringbootjpahibernate.repositories.UserRepository;
+import org.codiguin.webservicesspringbootjpahibernate.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
