ed82a2f4f94c9ba0e05e39ef2b5011b9dd9ba8e9
c0diguin <codiguin@git.com>
2025-08-07 23:53:03 -0300

    Exception handling - update

--- src/main/java/org/codiguin/webservicesspringbootjpahibernate/services/UserService.java
+++ src/main/java/org/codiguin/webservicesspringbootjpahibernate/services/UserService.java
@@ -1,5 +1,6 @@
 package org.codiguin.webservicesspringbootjpahibernate.services;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.codiguin.webservicesspringbootjpahibernate.entities.User;
 import org.codiguin.webservicesspringbootjpahibernate.repositories.UserRepository;
 import org.codiguin.webservicesspringbootjpahibernate.services.exceptions.DatabaseException;
@@ -43,9 +44,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
